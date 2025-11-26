aa5549813a5f4b65b940fbc2cf8cada8fef08232
itsamejms <james.twose@alliander.com>
2025-11-26 11:57:31 +0000

    fixing the relative path for assets issue

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/museu-sbras-website/",
   server: {
     host: "::",
     port: 5174,
