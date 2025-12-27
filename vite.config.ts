ce7d5e2086728a03f2d2c97e9e566e176f98af2f
itsamejms <james.twose@alliander.com>
2025-12-27 20:09:39 +0000

    updating to use my jms.rocks

--- vite.config.ts
+++ vite.config.ts
@@ -4,10 +4,9 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/museu-sbras-website/",
   server: {
     host: "::",
-    port: 5174,
+    port: 5173,
   },
   plugins: [react()].filter(Boolean),
   resolve: {
